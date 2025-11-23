f82f35dae5a1272d3c45ad802986dc6b2416e29d
KiRura <57588049+KiRura@users.noreply.github.com>
2025-11-24 02:28:31 +0900

    ビルドの通し方を変更

--- src/util/build.ts
+++ src/util/build.ts
@@ -2,23 +2,13 @@ import fs from "node:fs/promises";
 
 const root = "./src";
 const outdir = "./out";
-const dirs = ["script", "css"];
-const files = [];
-
-for (const dir of dirs) {
-	const pathes = (await fs.readdir(`${root}/${dir}`)).map(
-		(fileName) => `${root}/${dir}/${fileName}`,
-	);
-	files.push(...pathes);
-}
 
 console.log(
 	await Bun.build({
-		entrypoints: files,
+		entrypoints: [`${root}/script/index.ts`, `${root}/css/common.css`],
 		outdir,
 		minify: true,
 	}),
 );
 
-// fs.cp(`${root}/css/`, `${outdir}/css/`, { recursive: true });
-fs.cp(`${root}/manifest.json`, `${outdir}/manifest.json`);
+await fs.cp(`${root}/manifest.json`, `${outdir}/manifest.json`);
